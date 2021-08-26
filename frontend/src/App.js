db2386f7336a112aafc6aa96a4065b982dc53e83
AtaKaboudi <ata.kaboudi@gmail.com>
2021-08-26 20:21:10 +0100

    Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import Signup from "./components/form/Signup";
 import "./app.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="app">
-      <Signup />
-    </div>
-  );
+	return (
+		<div className="app">
+			<Router>
+				<Switch>
+					<Route path="/">
+						<Signup />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/components/form/Signup.jsx
+++ frontend/src/components/form/Signup.jsx
@@ -1,16 +1,27 @@
 import "./signup.scss";
-
+import logo from '../../cure.png'
+import { useState } from "react";
 export default function Signup() {
+
+  let [holderEmail,setHodlerEmail]= useState('Email')
+  let [holderPassword,setHodlerPassword]= useState('Password')
+  let [holderId,setHodlerId]= useState('CureId')
+  
   return (
     <div className="signup">
+      <img src={logo}/>
       <h1>digibionics</h1>
       <h2>Partner</h2>
         <form>
           <h1>Sign In</h1>
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="CureID" />
-          <button> Login </button>
+          <input type="text" placeholder={holderEmail} onClick={()=>{setHodlerEmail('')}} onChange={(e)=>setHodlerEmail(e.target.textContent)} />
+          <input type="password" placeholder={holderPassword} onClick={()=>{setHodlerPassword('')}} onChange={(e)=>setHodlerPassword(e.target.textContent)} />
+          <input type="password" placeholder={holderId} onClick={()=>{setHodlerId('')}} onChange={(e)=>setHodlerId(e.target.textContent)} />
+        <button> Login
+        <span class="material-icons">
+        chevron_right
+        </span>
+          </button>
         </form>
     </div>
   );
