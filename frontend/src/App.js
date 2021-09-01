f17b1529a30336a89a55bbbb4033038a3ac66aa9
AtaKaboudi <ata.kaboudi@gmail.com>
2021-09-02 00:20:08 +0100

    admin newPartnerPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import Company from "./components/pages/client/company/Company";
 import UpdatePatient from "./components/pages/client/updatePatient/updatePatient";
 import Home from "./components/pages/client/home/Home";
-
+import NewPartner from "./components/pages/admin/newPartner/NewPartner";
 import AdminHome from "./components/pages/admin/adminHome/AdminHome";
 import Signup from "./components/pages/client/signup/Signup";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
@@ -16,7 +16,7 @@ function App() {
 					<Route component={Company} path="/partner/company" />
 					<Route component={UpdatePatient} path="/partner/updatePatient" />
 					<Route component={Signup} path="/partner/signUp"></Route>
-
+					<Route component={NewPartner} path="/admin/newpartner"></Route>
 					<Route component={AdminHome} path="/admin"></Route>
 				</Switch>
 			</BrowserRouter>
--- frontend/src/components/pages/admin/newPartner/NewPartner.jsx
+++ frontend/src/components/pages/admin/newPartner/NewPartner.jsx
@@ -1,98 +1,106 @@
 import React from "react";
 import "./newPartner.scss";
 import PublishIcon from "@material-ui/icons/Publish";
+import Topbar from "../../admin/topbar/Topbar";
+import AdminSidebar from "../../admin/adminSidebar/AdminSidebar";
 
 export default function NewPartner() {
-  return (
-    <div className="partner">
-      <div className="partnerUpdate">
-        <span className="partnerUpdateTitle">New Partner</span>
-        <form className="partnerUpdateForm">
-          <div className="partnerUpdateItem">
-            <label>First Name</label>
-            <input
-              type="text"
-              placeholder="First Name"
-              className="partnerUpdateInput"
-            />
-          </div>
-          <div className="partnerUpdateItem">
-            <label>Phone number</label>
-            <input
-              type="text"
-              placeholder="+123456789"
-              className="partnerUpdateInput"
-            />
-          </div>
-          <div className="partnerUpdateItem">
-            <label>City</label>
-            <input
-              type="text"
-              placeholder="City"
-              className="partnerUpdateInput"
-            />
-          </div>
-          <div className="partnerUpdateItem">
-            <label>Postcode</label>
-            <input
-              type="text"
-              placeholder="31005"
-              className="partnerUpdateInput"
-            />
-          </div>
-        </form>
-        <div className="partnerFormRight">
-          <form className="partnerUpdateForm">
-            <div className="partnerUpdateItem">
-              <label>Last Name</label>
-              <input
-                type="text"
-                placeholder="Last Name"
-                className="partnerUpdateInput"
-              />
-            </div>
-            <div className="partnerUpdateItem">
-              <label>Email Adress</label>
-              <input
-                type="email"
-                placeholder="example@email.com"
-                className="partnerUpdateInput"
-              />
-            </div>
-            <div className="partnerUpdateItem">
-              <label>State/County</label>
-              <input
-                type="text"
-                placeholder="State/County"
-                className="partnerUpdateInput"
-              />
-            </div>
-            <div className="partnerUpdateItem">
-              <label>Country</label>
-              <input
-                type="text"
-                placeholder="Country"
-                className="partnerUpdateInput"
-              />
-            </div>
-          </form>
-        </div>
-        <div className="partnerUpdateUpload">
-          <img
-            className="partnerUpdateImg"
-            src="https://img.freepik.com/photos-gratuite/portrait-homme-blanc-isole_53876-40306.jpg?size=626&ext=jpg"
-            alt=""
-          />
-          <input type="file" id="file" style={{ display: "none" }} />
+	return (
+		<div className="partner">
+			<Topbar />
+			<div className="sectionContainer">
+				<AdminSidebar />
+				<div className="mainPannelContainer">
+					<div className="partnerUpdate">
+						<span className="partnerUpdateTitle">New Partner</span>
+						<form className="partnerUpdateForm">
+							<div className="partnerUpdateItem">
+								<label>First Name</label>
+								<input
+									type="text"
+									placeholder="First Name"
+									className="partnerUpdateInput"
+								/>
+							</div>
+							<div className="partnerUpdateItem">
+								<label>Phone number</label>
+								<input
+									type="text"
+									placeholder="+123456789"
+									className="partnerUpdateInput"
+								/>
+							</div>
+							<div className="partnerUpdateItem">
+								<label>City</label>
+								<input
+									type="text"
+									placeholder="City"
+									className="partnerUpdateInput"
+								/>
+							</div>
+							<div className="partnerUpdateItem">
+								<label>Postcode</label>
+								<input
+									type="text"
+									placeholder="31005"
+									className="partnerUpdateInput"
+								/>
+							</div>
+						</form>
+						<div className="partnerFormRight">
+							<form className="partnerUpdateForm">
+								<div className="partnerUpdateItem">
+									<label>Last Name</label>
+									<input
+										type="text"
+										placeholder="Last Name"
+										className="partnerUpdateInput"
+									/>
+								</div>
+								<div className="partnerUpdateItem">
+									<label>Email Adress</label>
+									<input
+										type="email"
+										placeholder="example@email.com"
+										className="partnerUpdateInput"
+									/>
+								</div>
+								<div className="partnerUpdateItem">
+									<label>State/County</label>
+									<input
+										type="text"
+										placeholder="State/County"
+										className="partnerUpdateInput"
+									/>
+								</div>
+								<div className="partnerUpdateItem">
+									<label>Country</label>
+									<input
+										type="text"
+										placeholder="Country"
+										className="partnerUpdateInput"
+									/>
+								</div>
+							</form>
+						</div>
+						<div className="partnerUpdateUpload">
+							<img
+								className="partnerUpdateImg"
+								src="https://img.freepik.com/photos-gratuite/portrait-homme-blanc-isole_53876-40306.jpg?size=626&ext=jpg"
+								alt=""
+							/>
+							<input type="file" id="file" style={{ display: "none" }} />
 
-          <button className="partnerUpdateButton">
-            <label htmlFor="file">
-              <PublishIcon className="partnerUpdateIcon" />
-            </label>
-            Upload
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+							<button className="partnerUpdateButton">
+								<label htmlFor="file">
+									<PublishIcon className="partnerUpdateIcon" />
+								</label>
+								Upload
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
